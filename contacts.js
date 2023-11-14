fd3cc3104a95c72a7e961e278e43b24d09778256
Viacheslav <gabon.qwa@gmail.com>
2023-11-14 21:34:23 +0200

    fix

--- contacts.js
+++ contacts.js
@@ -18,7 +18,7 @@ async function getContactById(contactId) {
 async function removeContact(contactId) {
   const contacts = await listContacts();
   const index = contacts.findIndex((contact) => contact.id === contactId);
-  if (index === -1) null;
+  if (index === -1) return null;
   const [result] = contacts.splice(index, 1);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return result;
@@ -34,7 +34,6 @@ async function addContact(name, email, phone) {
   };
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-  console.log(newContact);
   return newContact;
 }
 
